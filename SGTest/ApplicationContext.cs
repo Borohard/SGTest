0e463e19391aa9e1724358da6d74adb2aace02fe
Pavel Borodin <borpavbor122@gmail.com>
2023-08-22 15:52:04 +0500

    Добавьте файлы проекта.

---
+++ SGTest/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGTest
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Employee> Employees => Set<Employee>();
+        public DbSet<JobTitle> JobTitles => Set<JobTitle>();
+        public DbSet<Department> Departments => Set<Department>();
+
+        public ApplicationContext() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=SGTest;Username=postgres;Password=admin123");
+        }
+    }
+}
---
+++ SGTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using SGTest;
+using SGTest.Models;
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    JobTitle jobTitle1 = new JobTitle { Title = "Химик"};
+    JobTitle jobTitle2 = new JobTitle { Title = "Старшина" };
+    JobTitle jobTitle3 = new JobTitle { Title = "Звукорежиссёр" };
+
+    db.JobTitles.AddRange(jobTitle1, jobTitle2, jobTitle3);
+    db.SaveChanges();
+}
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    var jobTitles = db.JobTitles.ToList();
+    Console.WriteLine("Названия Должностей:");
+    foreach (JobTitle jobTitle in jobTitles)
+    {
+        Console.WriteLine($"{jobTitle.Title}");
+    }
+}
